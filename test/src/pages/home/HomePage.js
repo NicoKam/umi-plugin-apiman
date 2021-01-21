236455d5a26f27f22503e875ca3fa8899ea7afbd
NicoKam <331677620@qq.com>
2021-01-21 18:13:32 +0800

    add - page & utils

---
+++ test/src/pages/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './HomePage.less';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        This is Home
+      </div>
+    );
+  }
+}
+
+export default HomePage;
